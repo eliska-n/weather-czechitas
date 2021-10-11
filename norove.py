d4a4481c308c044c750a5a69ecf742222a4a2b3f
eliska <eliska.novotna@protonmail.com>
2021-10-11 23:37:10 +0200

    oscrapovala jsem prvni interval v usti nad labem

--- norove.py
+++ norove.py
@@ -3,13 +3,30 @@
 
 from metno_locationforecast import Place, Forecast
 import pandas as pd
+import datetime
 
 UserAgent = "novotna.elika@gmail.com"
 usti = Place("Usti nad Labem", 50.6833, 14.041, 375)
 usti_forecast = Forecast(usti, UserAgent)
 usti_forecast.update()
 
-keys = vars(usti_forecast.data).keys()
+first_interval = usti_forecast.data.intervals[0]
+start = str(first_interval.start_time)
+end = str(first_interval.end_time)
+duration = str(first_interval.duration)
+temperature = first_interval.variables['air_temperature']
+rain = first_interval.variables["precipitation_amount"]
+t_value = temperature.value
+r_value = rain.value
 
 
-print(usti_forecast.data.intervals)
+yrdict = {
+    "start": start, 
+    "end": end, 
+    "duration": duration, 
+    "air_temperature_C": t_value,
+    "precipitation_amount_mm": r_value
+    }
+
+
+print(yrdict)
